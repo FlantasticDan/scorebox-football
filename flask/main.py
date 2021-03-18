ba6bcea40a9b4db386656115a986e5791e8fbb8c
Daniel Flanagan <dancharley8@gmail.com>
2021-03-18 00:09:49 -0700

    ScoreBox Water Polo Port

---
+++ flask/main.py
@@ -0,0 +1,54 @@
+from flask import Flask, render_template, request, send_file
+from flask_socketio import SocketIO, emit
+
+from images import Logos
+from manager import FootballManager
+from bundle import bundle
+
+VERSION = 'v0.0.1 (03172021)'
+LOGOS = Logos()
+MANAGER = None # FootballManager
+
+app = Flask(__name__)
+socketio = SocketIO(app, cors_allowed_origins="*", async_mode='eventlet', logger=False, engineio_logger=False)
+
+@app.route('/setup')
+def setup():
+    return render_template('setup.html', version=VERSION)
+
+@app.route('/init', methods=['POST'])
+def initialize():
+    global LOGOS
+    global MANAGER
+    setup = request.form
+    MANAGER = FootballManager(**setup)
+    files = request.files
+    LOGOS.set_home(files['home_logo'].read(), files['home_logo'].filename)
+    LOGOS.set_visitor(files['visitor_logo'].read(), files['visitor_logo'].filename)
+    return 'OK'
+
+@app.route('/home')
+def home():
+    global LOGOS
+    output, mimetype = LOGOS.home()
+    return send_file(output, mimetype=mimetype, as_attachment=False)
+
+@app.route('/visitor')
+def visitor():
+    global LOGOS
+    output, mimetype = LOGOS.visitor()
+    return send_file(output, mimetype=mimetype, as_attachment=False)
+
+@app.route('/overlay')
+def overlay():
+    global MANAGER
+    if MANAGER:
+        return render_template('overlay.html', **MANAGER.overlay_export())
+
+@socketio.on('update')
+def update(payload):
+    return emit('update', payload, broadcast=True)
+
+if __name__ == '__main__':
+    bundle(app)
+    socketio.run(app, port=5000)
---
+++ flask/manager.py
@@ -0,0 +1,44 @@
+from secrets import token_hex
+from threading import Thread
+from typing import Dict
+
+import socketio
+
+from consoles.sports import Football
+
+class FootballManager:
+    '''Football Game State Manager'''
+    def __init__(self, home_team, home_mascot, home_color, visitor_team, visitor_mascot, visitor_color, com_port) -> None:
+        self.nonce = token_hex(6)
+
+        self.home_name = home_team
+        self.home_mascot = home_mascot
+        self.home_color = home_color
+        self.visitor_name = visitor_team
+        self.visitor_mascot = visitor_mascot
+        self.visitor_color = visitor_color
+
+        self.client = socketio.Client()
+        self.client_thread = Thread(target=self.socket_client)
+        self.client_thread.start()
+
+        self.console = Football(com_port)
+        self.console.on_update = self.updater
+    
+    def updater(self, game_state):
+        if self.client.connected:
+            self.client.emit('update', game_state)
+        
+    def socket_client(self):
+        self.client.connect('http://localhost:5000')
+        self.client.wait()
+
+    def overlay_export(self) -> Dict:
+        return {
+            'home_name': self.home_name,
+            'home_mascot': self.home_mascot,
+            'home_color': self.home_color,
+            'visitor_name': self.visitor_name,
+            'visitor_mascot': self.visitor_mascot,
+            'visitor_color': self.visitor_color,
+        }
