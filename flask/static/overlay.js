b37dac51255679955472c03aa5c4e5e6493d7745
Daniel Flanagan <37907774+FlantasticDan@users.noreply.github.com>
2021-03-19 00:13:42 -0700

    Possession Indicator

--- flask/static/overlay.js
+++ flask/static/overlay.js
@@ -5,6 +5,9 @@ const shotClock = document.getElementById('shot-clock')
 const period = document.getElementById('period')
 const downs = document.getElementById('downs')
 
+const homePossesion = document.getElementById('home-possesion')
+const visitorPossesion = document.getElementById('visitor-possesion')
+
 const socket = io()
 
 socket.on('update', payload => {
@@ -14,5 +17,16 @@ socket.on('update', payload => {
     shotClock.innerText = payload.play
     period.innerText = payload.quarter
     downs.innerText = `${payload.down} & ${payload.to_go}`
+
+    if (payload.home_possesion){
+        homePossesion.classList.remove('hide')
+        visitorPossesion.classList.add('hide')
+    }
+
+    if (payload.visitor_possesion){
+        homePossesion.classList.add('hide')
+        visitorPossesion.classList.remove('hide')
+    }
+
 })
 
